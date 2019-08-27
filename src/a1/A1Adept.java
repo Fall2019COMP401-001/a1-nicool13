7f9adf6eedfdb14c0e42e2a96446943460566ce3
730236688 <730236688@172.17.100.14>
2019-08-27 16:26:26 -0400

    wrote code

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,6 +1,8 @@
 package a1;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
+
 
 public class A1Adept {
 
@@ -10,5 +12,98 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		int items = scan.nextInt();
+		String storeItems[] = new String[items];
+		double itemCost[] = new double[items];
+		for (int i = 0; i < items; i++) {
+			storeItems[i] = scan.next();
+			itemCost[i] = scan.nextDouble();
+			}
+		
+		int ppl = scan.nextInt();
+		String pplName[] = new String[ppl];
+		double pplPay[] = new double[ppl];
+		for (int i = 0; i < ppl; i++) {
+			pplName[i] = scan.next() + " " + scan.next();
+			int count = scan.nextInt();
+			for (int j = 0; j < count; j++) {
+				int multiple = scan.nextInt();
+				String thing = scan.next();
+				for (int k = 0; k < storeItems.length; k++) {
+					if (storeItems[k].equals(thing)) {
+						double cost = itemCost[k];
+						pplPay[i] += multiple * cost;
+					}
+				}
+				
+			}
+		}
+		
+		
+		scan.close();
+		
+		
+		
+		biggest(pplName, pplPay);
+		smallest(pplName, pplPay);
+		average(pplPay, ppl);
+		
+	}	
+	
+	
+ static void biggest(String[] names, double[] pays) {
+	
+	int i = 0;
+	int person = 0;
+	while (i < names.length - 1) {
+		if (pays[i] < pays[i + 1]) {
+			person = i + 1;
+			i++;
+		} else {
+			person = i;
+			i++;
+		}
 	}
+	
+	DecimalFormat df2 = new DecimalFormat("0.00");
+	
+	System.out.println("Biggest: " + names[person] + " (" + df2.format(pays[person]) + ")");
+	
+	
+	}
+	
+	 static void smallest(String[] names, double[] pays) {
+		
+		int i = 0;
+		int person = 0;
+		while (i < names.length - 1) {
+			if (pays[i] > pays[i + 1]) {
+				person = i + 1;
+				i++;
+			} else {
+				person = i;
+				i++;
+			}
+		}
+		
+		DecimalFormat df2 = new DecimalFormat("0.00");
+
+		
+		System.out.println("Smallest: " + names[person] + " (" + df2.format(pays[person]) + ")");
+		
+		
+		}
+	 
+	 static void average(double[] pays, int ppl) {
+		 double sum = 0;
+		 for (int i = 0; i < pays.length; i++) {
+			 sum += pays[i];
+		 }
+		 
+		 DecimalFormat df2 = new DecimalFormat("0.00");
+		 
+		 System.out.println("Average: " + df2.format(sum/ppl));
+	 }
+	
 }
+			
