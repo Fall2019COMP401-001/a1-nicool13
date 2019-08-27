0a4905f0c497ad9ed391dd1b1c531b9a4b42ff8b
730236688 <730236688@172.17.100.14>
2019-08-26 20:49:11 -0400

    added project code

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,48 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
+
 		// Your code follows here.
+		
+		// Count holds the number of people the outer "for" loop will cycle through.
+		int count = scan.nextInt();
+		
+		// Outer for loop cycles through each customer and prints a line for each one.
+		for (int i = 0; i < count; i++) {
+		
+			// Variable holds first character of the first name.
+			char firstInitial = scan.next().charAt(0);
+			
+			// Variable holds last name
+			String lastName = scan.next();
+			
+			// Declare and initialize variable to hold total cost of all the items for each person
+			double total = 0;
+			
+			// Variable holds the amount of items the inner loop will cycle through  
+			int items = scan.nextInt();
+			
+			// Inner loop cycles through each item and adds their cost to the total 
+			for (int j = 0; j < items; j++) {
+				
+				// Holds the quantity of each item
+				int multiple = scan.nextInt();
+				
+				// Skips the item name 
+				scan.next();
+				
+				// Multiplies the cost of each item by its amount and add it to the total
+				total += multiple * scan.nextDouble();
+
+			}
+			  
+			// Prints the first initial, last name, and money paid for each person
+			System.out.println(firstInitial + ". " + lastName + ": " + total);
+			
+		}
+		
+		// Closes scan
+		scan.close();
 		
 	}
 }
