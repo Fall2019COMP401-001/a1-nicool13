24c8a0d8566c4ccbdc5387e7cd67f278d5e360ff
730236688 <730236688@172.17.100.14>
2019-08-28 11:23:12 -0400

    added comments to adept and jedi

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -12,27 +12,61 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		// Stores number of items found in the store
 		int items = scan.nextInt();
+		
+		// Creates string array that holds the name of each number
 		String storeItems[] = new String[items];
+		
+		// Creates double array that holds the price of each number at index that corresponds to the item name in the previous array
 		double itemCost[] = new double[items];
+		
+		// Scans through each item name and price to create both arrays
 		for (int i = 0; i < items; i++) {
 			storeItems[i] = scan.next();
 			itemCost[i] = scan.nextDouble();
 			}
 		
+		// Stores number of customers 
 		int ppl = scan.nextInt();
+		
+		// String array holds the name of each customer 
 		String pplName[] = new String[ppl];
+		
+		// Double array holds the amount of money each person spends at index that corresponds with their name in the previous array
 		double pplPay[] = new double[ppl];
+		
+		// Loops through each customer to create name array and determine the amount of money they spent to create the pplPay array
 		for (int i = 0; i < ppl; i++) {
+			
+			// Scans each person's first and last name and stores it in the string array
 			pplName[i] = scan.next() + " " + scan.next();
+			
+			// Stores the number of items each person bought
 			int count = scan.nextInt();
+			
+			// For loop cycles through each item
 			for (int j = 0; j < count; j++) {
+				
+				// Stores the amount of that item the person bought 
 				int multiple = scan.nextInt();
+				
+				// Stores the item's name
 				String thing = scan.next();
+				
+				// For loop cycles through the string array of the names of items found in the store to find the item that the person has bought and determine its index
 				for (int k = 0; k < storeItems.length; k++) {
+					
+					// Checks if the item at this index matches the item the customer is buying
 					if (storeItems[k].equals(thing)) {
+						
+						// If the item at this index does match the customer's item, this variable finds the cost of that item in the itemCost array at the same index that its name is found at in the itemName array
 						double cost = itemCost[k];
+						
+						// The amount of money the person has spent is determined by multiplying the cost of the item by the quantity of the item that the customer bought and adding it to however much money they have already spent
 						pplPay[i] += multiple * cost;
+						
+						// The if statement at line 61 cannot be true more than once so this line terminates the for loop by assigning to k a value that is outside of the for loop's range
 						k = storeItems.length;
 					}
 				}
@@ -40,37 +74,54 @@ public class A1Adept {
 			}
 		}
 		
-		
+		// Closes scan
 		scan.close();
 		
 		
-		
+		// Helper methods that determine the biggest/smallest spenders and the average amount of money spent and print the desired output
 		biggest(pplName, pplPay);
 		smallest(pplName, pplPay);
 		average(pplPay, ppl);
 		
 	}	
 	
+	/* biggest
+	 * 
+	 * Determines the biggest spender by comparing each value of a double array that holds how much each customer spent and storing the index at which the greatest value occurs
+	 * 
+	 * Input: String array of customer names, double array of customer's spending totals
+	 * 
+	 * Output: Prints a line that states the customer's name and their spending total at the determined index
+	 * 
+	*/
+	static void biggest(String[] names, double[] pays) {
 	
- static void biggest(String[] names, double[] pays) {
+		int i = 1;
+		int person = 0;
+		while (i <= names.length - 1) {
+			if (pays[i] < pays[person]) {
+				i++;
+			} else {
+				person = i;
+				i++;
+			} 
+		}
 	
-	int i = 1;
-	int person = 0;
-	while (i <= names.length - 1) {
-		if (pays[i] < pays[person]) {
-			i++;
-		} else {
-			person = i;
-			i++;
-		} 
+		DecimalFormat df2 = new DecimalFormat("0.00");
+	
+		System.out.println("Biggest: " + names[person] + " (" + df2.format(pays[person]) + ")");
+	
 	}
 	
-	DecimalFormat df2 = new DecimalFormat("0.00");
-	
-	System.out.println("Biggest: " + names[person] + " (" + df2.format(pays[person]) + ")");
-	
-	
-	}
+	/* smallest
+	 * 
+	 * Determines the smallest spender by comparing each value of a double array that holds how much each customer spent and storing the index at which the smallest value occurs
+	 * 
+	 * Input: String array of customer names, double array of customer's spending totals
+	 * 
+	 * Output: Prints a line that states the customer's name and their spending total at the determined index
+	 * 
+	*/
 	
 	 static void smallest(String[] names, double[] pays) {
 		
@@ -92,6 +143,16 @@ public class A1Adept {
 		
 		
 		}
+	 
+	 	/* average
+		 * 
+		 * Determines the average amount of money spent by customers by determining the sum of all of the customers' spending totals and dividing by the number of customers
+		 * 
+		 * Input: double array of customer's spending totals, integer number of customers
+		 * 
+		 * Output: Prints a line that states the average spending between all of the customers
+		 * 
+		*/
 	 
 	 static void average(double[] pays, int ppl) {
 		 double sum = 0;
--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,32 +10,67 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		// Stores the number of items found at the store
 		int items = scan.nextInt();
+		
+		// String array stores the name of each item in the store
 		String itemName[] = new String[items];
+		
+		// Integer array stores the amount of each item purchased at the same index as its name in itemName[]
 		int itemAmt[] = new int[items];
+		
+		// String array stores the name of the item bought by the current customer
 		String pplItems[] = new String[items];
+		
+		// Integer array stores the number of people that purchased each item at the same index as its name in itemName[]
 		int pplAmt[] = new int[items];
+		
+		// Loops through each item, stores its name in itemName and skips over its price
 		for (int i = 0; i < items; i++) {
 			itemName[i] = scan.next();
 			scan.nextDouble();
 		}
 		
-		
+		// Stores the number of customers
 		int ppl = scan.nextInt();
+		
+		// Loops through each customer, skips over their first and last name, adds the amount/type of items they bought to itemAmt[] and pplAmt[]
 		for (int i = 0; i < ppl; i++) {
+			
+			// Skip first and last name
 			scan.next();
 			scan.next();
+			
+			// Stores the number of items the customer bought
 			int count = scan.nextInt();
+			
+			// Loops through each item
 			for (int j = 0; j < count; j++) {
+				
+				// Stores the amount of the item the customer bought
 				int amt = scan.nextInt();
+				
+				// Stores the name of the item the customer bought
 				String thing = scan.next();
+				
+				// Adds the name of the item to the array that holds the customer's purchased items
 				pplItems[j] = thing;
+				
+				// Loops through the array that holds the name of each item found at the store
 				for (int k = 0; k < itemName.length; k++) {
+					
+					// If the item present at the current index matches the one purchased by the customer, the amount of the item purchased is added to its amount stored in the itemAmt[] at the same index as its name in storeItems[]
 					if (itemName[k].equals(thing)) {
+						
+						// Amount of the item is added to the current amount of that item in the array
 						itemAmt[k] += amt;
-						if (ItemCheck(pplItems, j, thing)) {
+						
+						// If itemCheck returns true the amount of people that purchased this item is increased by 1--stored at the same index as the item name and amount
+						if (itemCheck(pplItems, j, thing)) {
 							pplAmt[k] += 1;
 						}
+						
+						// Ends the loop to move on to the next item the customer has purchased
 						k = itemName.length;
 					}
 				}
@@ -43,8 +78,10 @@ public class A1Jedi {
 			
 		}
 		
+		// Closes scan
 		scan.close();
 		
+		// Loops through each item in storeItems[] and prints desired output detailing how many people bought each item, the amount of item that was bought, and the item name -- all of these values occur at the same index in different arrays
 		for (int n = 0; n < items; n++) {
 			if (pplAmt[n] == 0) {
 				System.out.println("No customers bought " + itemName[n]);
@@ -56,7 +93,18 @@ public class A1Jedi {
 			
 	}
 	
-	public static boolean ItemCheck(String[] pplItems, int j, String thing) {
+	/* itemCheck
+	 * 
+	 * Cycles backwards through the customer's list of items purchased starting at the index of the current item and attempts to match the given item name with the previous items on the customers list
+	 * In the case of one person having the same item twice on their list, this method prevents counting that person twice and falsely indicating that two separate people purchased that item when reporting the number of people that bought a certain item. 
+	 * 
+	 * Input: String array of the items the customer has purchased, integer that specifies the index at which this item occurs in the array that holds the items this customer has purchased, String of the item's name
+	 * 
+	 * Output: returns true if the item on this person's list is appearing for the first time, and returns false if it has already appeared
+	 * 
+	*/
+	
+	public static boolean itemCheck(String[] pplItems, int j, String thing) {
 		for (int i = j - 1; i >= 0; i--) {
 				if (pplItems[i].equals(thing)) {
 					return false;
@@ -64,5 +112,5 @@ public class A1Jedi {
 			}
 		return true;
 		}
-		}
+}
 
